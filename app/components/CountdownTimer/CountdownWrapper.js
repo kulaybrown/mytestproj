6ec6cf256c298f479750c17342be548f42eb0540
Angelo Lagumbay <angelol@istackmanila.com>
2018-08-03 16:51:52 +0800

    sdfds

--- app/components/CountdownTimer/CountdownWrapper.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-const CountdownWrapper = styled.div`
-  width: 490px;
-  margin: auto;
-`;
-
-export default CountdownWrapper;
--- internals/templates/common/actions.js
+++
@@ -1,9 +0,0 @@
-import {
-  TOGGLE_SIDE_NAV,
-} from './constants';
-
-export function toggleSideNav() {
-  return {
-    type: TOGGLE_SIDE_NAV,
-  };
-}
